1041473ad7ace2e89c5ffc5929ef01f7f20ebe7a
hudson-uchoa <uchoa.hudson1@gmail.com>
2025-04-05 14:08:48 +0100

    feat: migracoes das tabelas contacts e peoples feita

---
+++ database/migrations/2025_04_05_124731_create_contacts.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('country_code', 5);
+            $table->string('number', 15);
+            $table->foreignId('people_id')->references('id')->on('peoples')->onDelete('cascade');
+            $table->timestamps();
+
+            $table->unique(['country_code', 'number']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
